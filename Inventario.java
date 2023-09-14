1be4c0d056b8b9d33d12376ad5b1ec278d662ca9
Henrique <riqueazevedo87@gmail.com>
2023-09-13 21:35:01 -0300

    Classe inventário adicionada

--- Inventario.java
+++ Inventario.java
@@ -1,4 +1,30 @@
-
 public class Inventario {
+    private Carta[] carta = new Carta[200];
+    private int nivelAtual;
+    private int cardCoins;
+
+    public Inventario(Carta[] carta, int nivelAtual, int cardCoins) {
+        this.carta = carta;
+        this.nivelAtual = nivelAtual;
+        this.cardCoins = cardCoins;
+    }
+    public Carta[] getCarta() {
+        return carta;
+    }
+    public void setCarta(Carta[] carta) {
+        this.carta = carta;
+    }
+    public int getNivel() {
+        return nivelAtual;
+    }
+    public void setNivel(int nivelAtual) {
+        this.nivelAtual = nivelAtual;
+    }
+    public int getCards() {
+        return cardCoins;
+    }
+    public void setCards(int cardCoins) {
+        this.cardCoins = cardCoins;
+    }
 
 }
