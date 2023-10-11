0acbf32dadbdf1270f03cc8109a9165562434be2
Henrique Azevedo <riqueazevedo87@gmail.com>
2023-10-10 22:38:08 -0300

    Carta única

--- CartaUnica.java
+++ CartaUnica.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class CartaUnica extends Carta {
     private int ataque;
     private int defesa;
@@ -10,10 +12,32 @@ public class CartaUnica extends Carta {
         this.defesa += 1;
         this.habilidadePadrao += ", " + gerarHabilidadeAleatoria();
     }
+    public int getAtaque() {
+        return ataque;
+    }
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+    public int getDefesa() {
+        return defesa;
+    }
+    public void setDefesa(int defesa) {
+        this.defesa = defesa;
+    }
+    public String getHabilidadePadrao() {
+        return habilidadePadrao;
+    }
+    public void setHabilidadePadrao(String habilidadePadrao) {
+        this.habilidadePadrao = habilidadePadrao;
+    }
 
 
 
-    private String gerarHabilidadeAleatoria() {
-        return null;
+    private Habilidade gerarHabilidadeAleatoria() {
+        Habilidade[] habilidades = Habilidade.values();
+        Random random = new Random();
+        int index = random.nextInt(habilidades.length);
+        return habilidades[index];
+ 
     }
 }
