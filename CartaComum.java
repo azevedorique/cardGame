3db30d301deff82c6edb84dcb68665a5e1e160d9
Henrique Azevedo <riqueazevedo87@gmail.com>
2023-10-11 23:56:43 -0300

    Carta comum updates

--- CartaComum.java
+++ CartaComum.java
@@ -10,6 +10,25 @@ public class CartaComum extends Carta {
         this.defesa = 1;
         this.habilidadePadrao = gerarHabilidadeAleatoria();
     }
+    public int getAtaque() {
+        return ataque;
+    }
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+    public int getDefesa() {
+        return defesa;
+    }
+    public void setDefesa(int defesa) {
+        this.defesa = defesa;
+    }
+    public String getHabilidadePadrao() {
+        return habilidadePadrao;
+    }
+    public void setHabilidadePadrao(String habilidadePadrao) {
+        this.habilidadePadrao = habilidadePadrao;
+    }
+
 
     private String gerarHabilidadeAleatoria() {
         String[] habilidades = {"Habilidade1", "Habilidade2", "Habilidade3"};
